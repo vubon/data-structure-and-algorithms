9d2e4b3c0dcaf8e7e928397fb75f2f85529fc310
vubon <vubon.roy@gmail.com>
2019-03-18 18:03:10 +0600

    Modified docs for round robin algo

---
+++ algorithms/CPU Scheduling Algorithms/round_robin/round_robin.py
@@ -0,0 +1,23 @@
+"""
+Round Robin Algorithms
+"""
+RQ = []  # Ready Queue
+RT = []  # Remaining Time
+CT = []  # Completion Time
+
+
+def average_waiting_time(processes, bt, qt, at):
+    """
+    :param processes:
+    :param bt:
+    :param qt:
+    :param at:
+    :return: None
+    """
+
+
+if __name__ == '__main__':
+    processes_no = [1, 2, 3, 4, 5]
+    burst_time = [7, 5, 2, 3, 8]
+    quantum_time = 3
+    arrival_time = [0, 5, 10, 7, 3]
