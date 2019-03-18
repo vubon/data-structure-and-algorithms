b50dd97793165ead39367fa6c73940f1a160c72f
vubon <vubon.roy@gmail.com>
2019-03-18 11:40:45 +0600

    FCFS

--- algorithms/CPU Scheduling Algorithms/FCFS/fcfs.py
+++ algorithms/CPU Scheduling Algorithms/FCFS/fcfs.py
@@ -3,9 +3,8 @@ FCFS means  First Come First Serve
 """
 
 
-def finding_waiting_time(processes, lengths, br, wt):
+def finding_waiting_time(lengths, br, wt):
     """
-    :param processes:
     :param lengths:
     :param br:
     :param wt:
@@ -18,7 +17,59 @@ def finding_waiting_time(processes, lengths, br, wt):
         wt[i] = br[i - 1] + wt[i - 1]
 
 
-total_processes = [1,2,3,4,5]
-burst_time = [21, 10, 30, 15, 25]
-total_lengths = len(total_processes)
-print(finding_waiting_time(total_processes, total_lengths, burst_time,))
+# Function to calculate turn
+# around time
+def find_turn_around_time( n, bt, wt, tat):
+    # calculating turnaround
+    # time by adding bt[i] + wt[i]
+    for i in range(n):
+        tat[i] = bt[i] + wt[i]
+
+    # Function to calculate
+
+
+def find_avg_time(n, bt):
+    """
+    :param n:
+    :param bt:
+    :return:
+    """
+    wt = [0] * n
+    tat = [0] * n
+    total_wt = 0
+    total_tat = 0
+
+    # Function to find waiting
+    # time of all processes
+    finding_waiting_time(n, bt, wt)
+
+    # Function to find turn around
+    # time for all processes
+    find_turn_around_time(n, bt, wt, tat)
+
+    # Display processes along
+    # with all details
+    print("Processes Burst time " + " Waiting time " + " Turn around time")
+
+    # Calculate total waiting time
+    # and total turn around time
+    for i in range(n):
+        total_wt = total_wt + wt[i]
+        total_tat = total_tat + tat[i]
+        print(" " + str(i + 1) + "\t\t\t" + str(bt[i]) + "\t\t\t " + str(wt[i]) + "\t\t\t " + str(tat[i]))
+
+    print("Average waiting time = " +
+          str(total_wt / n))
+    print("Average turn around time = " +
+          str(total_tat / n))
+
+
+if __name__ == "__main__":
+    # process id's
+    total_processes = [1, 2, 3, 4, 5]
+    # burst time
+    burst_time = [21, 10, 30, 15, 25]
+    # all processes lengths
+    total_lengths = len(total_processes)
+
+    find_avg_time(total_lengths, burst_time)
